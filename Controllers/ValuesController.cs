8b639a0d2a3b28b967518fc500d638ac620751ce
Letizia <letizia.latini@studenti.unicam.it>
2025-10-24 18:47:00 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BudgetTracker.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MovimentiController : ControllerBase
+    {
+        private static List<Movimento> movimenti = new List<Movimento>
+        {
+            new Movimento { Id = 1, Descrizione = "Stipendio", Importo = 1500, Entrata = true },
+            new Movimento { Id = 2, Descrizione = "Spesa alimentare", Importo = 80, Entrata = false }
+        };
+
+        [HttpGet]
+        public IActionResult Get() => Ok(movimenti);
+
+        [HttpPost]
+        public IActionResult Post(Movimento nuovo)
+        {
+            nuovo.Id = movimenti.Count + 1;
+            movimenti.Add(nuovo);
+            return CreatedAtAction(nameof(Get), new { id = nuovo.Id }, nuovo);
+        }
+    }
+
+    public class Movimento
+    {
+        public int Id { get; set; }
+        public string Descrizione { get; set; }
+        public double Importo { get; set; }
+        public bool Entrata { get; set; }
+    }
+}
